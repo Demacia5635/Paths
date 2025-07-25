5b58c85d2ac983f398f394b166d17236e6749dbf
ipb933 <idoperlman9@gmail.com>
2025-07-25 21:52:57 +0300

    added SparkMax code

--- src/main/java/frc/robot/utils/LimitSwitchConfig.java
+++ src/main/java/frc/robot/utils/LimitSwitchConfig.java
@@ -4,5 +4,14 @@ public class LimitSwitchConfig {
     public int channel;
     public String name;
 
-    
+    /** 
+     * Constructor
+     * @param id - canbus ID
+     * @param canbus - Name of canbus
+     * @param name - name of Pigeon for logging
+     */
+    public LimitSwitchConfig(int channel, String name) {
+        this.channel = channel;
+        this.name = name;
+    }
 }
