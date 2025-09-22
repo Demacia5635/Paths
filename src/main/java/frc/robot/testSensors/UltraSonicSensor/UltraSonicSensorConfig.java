724552d00f74baf7e4c4358b665f25d8b1608fde
haghanuka <idan12343567@gmail.com>
2025-09-22 11:46:18 +0300

    made the cnofig

--- src/main/java/frc/robot/testSensors/UltraSonicSensor/UltraSonicSensorConfig.java
+++ src/main/java/frc/robot/testSensors/UltraSonicSensor/UltraSonicSensorConfig.java
@@ -1,9 +1,12 @@
+package frc.robot.testSensors.UltraSonicSensor;
+import frc.demacia.utils.Sensors.AnalogEncoderConfig;
+import frc.demacia.utils.Sensors.AnalogSensorConfig;
 
-package frc.demacia.utils.Sensors;
-
-public class UltraSonicSensorConfig {
+public class UltraSonicSensorConfig extends AnalogSensorConfig<UltraSonicSensorConfig> {
 public UltraSonicSensorConfig(int channel, String name) {
     super(channel, name);
+    
     }
 
 }
+
