215396e6233b1bc897fed06306c2c68a1bb5578e
haghanuka <idan12343567@gmail.com>
2025-09-22 10:57:14 +0300

    started the config and files in ultrasonic

---
+++ src/main/java/frc/robot/testSensors/UltraSonicSensor/Constants.java
@@ -0,0 +1,7 @@
+
+package frc.robot.testSensors.UltraSonicSensor;
+import frc.demacia.utils.Motors.BaseMotorConfig.Canbus;
+
+public class Constants {
+
+}
---
+++ src/main/java/frc/robot/testSensors/UltraSonicSensor/UltraSonicSensorConfig.java
@@ -0,0 +1,9 @@
+
+package frc.demacia.utils.Sensors;
+
+public class UltraSonicSensorConfig {
+public UltraSonicSensorConfig(int channel, String name) {
+    super(channel, name);
+    }
+
+}
