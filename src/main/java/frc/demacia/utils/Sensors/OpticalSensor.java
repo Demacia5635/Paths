3b7e648222f2edd92e58c0f253d08781eb1c6834
UriMeyTal <urimeytal100@gmail.com>
2025-09-29 17:05:38 +0300

    did some pizza code

--- src/main/java/frc/demacia/utils/Sensors/OpticalSensor.java
+++ src/main/java/frc/demacia/utils/Sensors/OpticalSensor.java
@@ -1,6 +1,32 @@
 package frc.demacia.utils.Sensors;
+import edu.wpi.first.wpilibj.AnalogInput;
 
-public class OpticalSensor {
-    
+public class OpticalSensor extends AnalogInput implements SensorInterface {
+    private final OpticalSensorConfig config;
+    private final String name;
+
+    public OpticalSensor(OpticalSensorConfig config) {
+        super(config.port);
+        this.config = config;
+        this.name = config.name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getValue() {
+        return super.getValue();
+    }
+
+    public double get(){
+        return getValue();
+    }
+
+    public double getVoltage() {
+        return super.getVoltage();
+    }
+
+ 
 
 }
