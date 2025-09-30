b6bd3194eb53b5950c680bcfd06d3d8e98fb93fa
haghanuka <idan12343567@gmail.com>
2025-09-30 16:25:31 +0300

    fixed the config

--- src/main/java/frc/demacia/utils/Sensors/OpticalSensorConfig.java
+++ src/main/java/frc/demacia/utils/Sensors/OpticalSensorConfig.java
@@ -1,9 +1,9 @@
 package frc.demacia.utils.Sensors;
 
-public class OpticalSensorConfig extends AnalogSensorConfig {
+public class OpticalSensorConfig extends AnalogSensorConfig<OpticalSensorConfig> {
 
     public OpticalSensorConfig(String name, int port) {
-        super(name, port);
+        super(port, name);
     }
 
 }
