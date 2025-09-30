012b124a1f9f47487f04a15d8b7d4d5dd24e498b
haghanuka <idan12343567@gmail.com>
2025-09-30 17:57:24 +0300

    config

--- src/main/java/frc/demacia/utils/Sensors/LidarSensorConfig.java
+++ src/main/java/frc/demacia/utils/Sensors/LidarSensorConfig.java
@@ -1,5 +1,8 @@
 package frc.demacia.utils.Sensors;
 
-public class LidarSensorConfig {
+public class LidarSensorConfig extends AnalogSensorConfig<LidarSensorConfig> {
+    public LidarSensorConfig(String name, int port) {
+        super(port, name);
+    }
 
 }
