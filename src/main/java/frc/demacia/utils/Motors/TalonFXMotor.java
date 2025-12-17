bcf2f832bc9ce0540be7897da8c5c4574a597faf
ipb933 <idoperlman9@gmail.com>
2025-12-17 21:09:53 +0200

    talon motor

--- src/main/java/frc/demacia/utils/Motors/TalonFXMotor.java
+++ src/main/java/frc/demacia/utils/Motors/TalonFXMotor.java
@@ -168,7 +168,7 @@ public class TalonFXMotor extends TalonFX implements MotorInterface {
             currentSignal.getSignal(),
             closedLoopErrorSignal.getSignal(),
             closedLoopSPSignal.getSignal(),
-            }).withLogLevel(LogLevel.LOG_AND_NT_NOT_IN_COMP)
+            }).withLogLevel(LogLevel.LOG_ONLY_NOT_IN_COMP)
             .WithIsMotor().build();
         LogManager.addEntry(name + " ControlMode", 
             () -> getCurrentControlMode())
